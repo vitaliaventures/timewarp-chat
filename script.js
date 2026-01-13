02b15caf5422acbed5555cf86eb2b1b96341d872
vitaliaventures <leaveapositivelegacy@gmail.com>
2026-01-12 20:42:13 -0500

    script.js

--- script.js
+++ script.js
@@ -521,6 +521,16 @@ function toArabicDigits(str) {
 }
 
 
+function fromArabicDigits(str) {
+  const map = {
+    "٠":"0","١":"1","٢":"2","٣":"3","٤":"4",
+    "٥":"5","٦":"6","٧":"7","٨":"8","٩":"9"
+  };
+  return str.replace(/[٠-٩]/g, d => map[d]);
+}
+
+
+
 
 function formatTime(sec) {
   const m = Math.floor(sec / 60);
