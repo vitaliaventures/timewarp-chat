ecb15d1d9968deb9afd32ecbff587aa47e6f1d0e
vitaliaventures <leaveapositivelegacy@gmail.com>
2026-01-18 05:30:21 -0500

    script.js

--- script.js
+++ script.js
@@ -416,7 +416,7 @@ function fromArabicDigits(str) {
 
 // --- Message TTL parser (mm:ss or ss)
 function parseTTL() {
-  let ttlInput = document.getElementById("ttl-input")?.value || "10:00";
+  let ttlInput = document.getElementById("ttl-input")?.value || "01:00";
 
   // 🔥 convertir números árabes → latinos antes de parsear
   ttlInput = fromArabicDigits(ttlInput);
