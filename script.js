dad5c18bfe73d4eb5ec0424c2849523debd91b64
vitaliaventures <leaveapositivelegacy@gmail.com>
2026-01-21 23:18:10 -0500

    script.js

--- script.js
+++ script.js
@@ -1048,24 +1048,6 @@ actionMenu.style.display = "block";
 
   // Limpiar interval anterior si existía
   if (div.countdownTimer) clearInterval(div.countdownTimer);
-
-  div.countdownTimer = setInterval(() => {
-    remaining--;
-    span.textContent = formatTime(remaining);
-
-    const percent = (remaining / total) * 100;
-    fill.style.width = percent + "%";
-
-    if (percent > 30) fill.style.background = "#22c55e"; // green
-    else if (percent > 10) fill.style.background = "#facc15"; // yellow
-    else fill.style.background = "#ef4444"; // red
-
-    if (remaining <= 0) {
-      clearInterval(div.countdownTimer);
-      div.remove();
-      remove(snap.ref);
-    }
-  }, 1000);
 });
 
 
